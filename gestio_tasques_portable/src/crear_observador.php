49a0df845c999a9745459ea36ea758503efb4fa9
IkerCaballero <icaballero@institutmvm.cat>
2026-04-28 19:43:56 +0200

    Delete gestio_tasques_portable directory

--- gestio_tasques_portable/src/crear_observador.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "models/db.php";
-
-$db = conectar();
-$db->query("INSERT INTO usuarios (usuario, password, rol)
-            VALUES ('observador', '".password_hash("observador123", PASSWORD_DEFAULT)."', 'observador')");
